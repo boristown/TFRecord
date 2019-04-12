9a5db25825ecf47f4387da84360443d95a99b576
boristown <boristown@gmail.com>
2019-04-12 11:07:38 +0800

    迭代5：显示TFRecord数据

--- TFRecord/TFRecord_Reader.py
+++ TFRecord/TFRecord_Reader.py
@@ -13,8 +13,8 @@ def read_and_decode(filename): # 读入dog_train.tfrecords
     _, serialized_example = reader.read(filename_queue)#返回文件名和文件
     features = tf.parse_single_example(serialized_example,
                                        features={
-                                           'label': tf.FixedLenFeature([], tf.int64),
-                                           'mg_raw' : tf.FixedLenFeature([], tf.float32),
+                                           'label': tf.FixedLenFeature([1], tf.int64, default_value=[0]),
+                                           'mg_raw' : tf.FixedLenFeature([2], tf.float32, default_value=[0.0,0.0]),
                                            #'img_raw' : tf.FixedLenFeature([], tf.string),
                                        })#将image数据和label取出来
 
@@ -29,6 +29,12 @@ img, label = read_and_decode("dog_train.tfrecords") #要生成的文件
 print( "label=" + str(label) )
 print( "img=" + str(img) )
 with tf.Session() as sess: #开始一个会话
+    init_op = tf.initialize_all_variables()
+    sess.run(init_op)
+    coord=tf.train.Coordinator()
+    threads= tf.train.start_queue_runners(coord=coord)
     img2, label2 = sess.run([img,label])#在会话中取出image和label
     print( "label2=" + str(label2) )
     print( "img2=" + str(img2) )
+    coord.request_stop()
+    coord.join(threads)
