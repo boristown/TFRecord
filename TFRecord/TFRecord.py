5638b481a4b25654c32518e2fcbb1a410faee331
boristown <boristown@gmail.com>
2019-04-12 11:26:29 +0800

    迭代7：设置prices与label

--- TFRecord/TFRecord.py
+++ TFRecord/TFRecord.py
@@ -1,24 +1,15 @@
 import os 
 import tensorflow as tf 
-#from PIL import Image  #注意Image,后面会用到
-#import matplotlib.pyplot as plt 
 import numpy as np
 
 cwd= 'data/' 
 classes={'husky','chihuahua'} #人为 设定 2 类
-writer= tf.python_io.TFRecordWriter("dog_train.tfrecords") #要生成的文件
+writer= tf.python_io.TFRecordWriter("zero_train.tfrecords") #要生成的文件
 
 for index,name in enumerate(classes):
-    #class_path=cwd+name+'\\'
-    #for img_name in os.listdir(class_path): 
-    #    img_path=class_path+img_name #每一个图片的地址
-
-    #    img=Image.open(img_path)
-    #    img= img.resize((128,128))
-    #    img_raw=img.tobytes()#将图片转化为二进制格式
     example = tf.train.Example(features=tf.train.Features(feature={
+        "prices": tf.train.Feature(float_list=tf.train.FloatList(value=[0.1,0.2,0.3,0.4,0.5])),
         "label": tf.train.Feature(int64_list=tf.train.Int64List(value=[index])),
-        "mg_raw": tf.train.Feature(float_list=tf.train.FloatList(value=[0.5,0.6]))
         #'img_raw': tf.train.Feature(bytes_list=tf.train.BytesList(value=[img_raw]))
     })) #example对象对label和image数据进行封装
     writer.write(example.SerializeToString())  #序列化为字符串
--- TFRecord/TFRecord_Reader.py
+++ TFRecord/TFRecord_Reader.py
@@ -1,7 +1,5 @@
 import os 
 import tensorflow as tf 
-#from PIL import Image  #注意Image,后面会用到
-#import matplotlib.pyplot as plt 
 import numpy as np
 
 
@@ -13,29 +11,26 @@ def read_and_decode(filename): # 读入dog_train.tfrecords
     _, serialized_example = reader.read(filename_queue)#返回文件名和文件
     features = tf.parse_single_example(serialized_example,
                                        features={
+                                           'prices' : tf.FixedLenFeature([5], tf.float32, default_value=[0.0]*5),
                                            'label': tf.FixedLenFeature([1], tf.int64, default_value=[0]),
-                                           'mg_raw' : tf.FixedLenFeature([2], tf.float32, default_value=[0.0,0.0]),
                                            #'img_raw' : tf.FixedLenFeature([], tf.string),
                                        })#将image数据和label取出来
 
-    #img = tf.decode_raw(features['img_raw'], tf.uint8)
-    #img = tf.reshape(img, [128, 128, 3])  #reshape为128*128的3通道图片
-    #img = tf.cast(img, tf.float32) * (1. / 255) - 0.5 #在流中抛出img张量
     label = tf.cast(features['label'], tf.int32) #在流中抛出label张量
-    img = tf.cast(features['mg_raw'], tf.float32) #在流中抛出label张量
-    return img, label
+    prices = tf.cast(features['prices'], tf.float32) #在流中抛出label张量
+    return prices, label
 
-img, label = read_and_decode("dog_train.tfrecords") #要生成的文件
+prices, label = read_and_decode("zero_train.tfrecords") #要生成的文件
 print( "label=" + str(label) )
-print( "img=" + str(img) )
+print( "prices=" + str(prices) )
 with tf.Session() as sess: #开始一个会话
     init_op = tf.initialize_all_variables()
     sess.run(init_op)
     coord=tf.train.Coordinator()
     threads= tf.train.start_queue_runners(coord=coord)
     for i in range(2):
-        img2, label2 = sess.run([img,label])#在会话中取出image和label
-        print( "label2=" + str(label2) )
-        print( "img2=" + str(img2) )
+        prices2, label2 = sess.run([prices,label])#在会话中取出image和label
+        print( "label=" + str(label2) )
+        print( "prices=" + str(prices2) )
     coord.request_stop()
     coord.join(threads)
